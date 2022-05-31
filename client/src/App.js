63b3acdcadfdde0173e4758bdbb6312cbb767131
Kevin Poorman <kjp@codefriar.com>
2022-05-31 13:23:51 -0400

    now with a table.

--- client/src/App.js
+++ client/src/App.js
@@ -4,12 +4,10 @@ import Toolbar from '@material-ui/core/Toolbar'
 import TypoGraphy from '@material-ui/core/Typography'
 import Card from '@material-ui/core/Card'
 import CardContent from '@material-ui/core/CardContent'
-import List from '@material-ui/core/List'
-import ListItem from '@material-ui/core/ListItem'
-import ListItemText from '@material-ui/core/ListItemText'
 import TextField from '@material-ui/core/TextField'
 import Button from '@material-ui/core/Button'
 import BuildEnv from './buildEnv.js'
+import ResultsDataTable from './ResultsDataTable.js'
 import './App.css'
 
 function App() {
@@ -18,18 +16,20 @@ function App() {
   const handleChange = (event) => setSearchTerm(event.target.value)
   const handleSearch = async (event) => {
     event.preventDefault()
-    const searchResults = await fetch(
-      `${BuildEnv()}/restaurant/search/${searchTerm}`
-    )
-
-    setResults(await searchResults.json())
+    setSearchTerm(event.target.value)
+    if (searchTerm.length > 0) {
+      const searchResults = await fetch(
+        `${BuildEnv()}/restaurant/search/${searchTerm}`
+      )
+      setResults(await searchResults.json())
+    }
   }
   return (
     <div className="App">
       <AppBar color="primary" position="static">
         <Toolbar>
           <TypoGraphy variant="h3" color="inherit">
-            Fastify, React, Node & Mongo Atlas Search
+            NYC Restaurant Search
           </TypoGraphy>
         </Toolbar>
       </AppBar>
@@ -42,6 +42,9 @@ function App() {
               variant="standard"
               placeholder="Search Term"
               value={searchTerm}
+              onInput={(e) => {
+                handleSearch(e)
+              }}
               onChange={handleChange}
             />
             <Button
@@ -58,17 +61,7 @@ function App() {
       </Card>
       <Card>
         <CardContent>
-          Results
-          <List>
-            {results.map((restaurant) => (
-              <ListItem>
-                <ListItemText
-                  primary={restaurant.name}
-                  secondary={restaurant.cuisine}
-                />
-              </ListItem>
-            ))}
-          </List>
+          <ResultsDataTable results={results}></ResultsDataTable>
         </CardContent>
       </Card>
     </div>
--- server/controllers/Restaurant.js
+++ server/controllers/Restaurant.js
@@ -10,15 +10,99 @@ const getAllRecords = async (req, res) => {
 
 // SEARCH using Atlas Search
 const search = async (req, res) => {
-  var data = await restaurants.aggregate().search({
-    index: 'cuisine',
-    text: {
-      query: req.params.term,
-      path: {
-        wildcard: '*'
+  var data = await restaurants
+    .aggregate()
+    .search({
+      autocomplete: {
+        // adding facets to add categories
+        query: req.params.term,
+        fuzzy: {
+          maxEdits: 2,
+          prefixLength: 3
+        },
+        path: 'name'
       }
-    }
-  })
+    })
+    .addFields({
+      avgScore: {
+        $map: {
+          input: '$grades.grade',
+          in: {
+            $switch: {
+              branches: [
+                {
+                  case: {
+                    $eq: ['$$this', 'A']
+                  },
+                  then: 5
+                },
+                {
+                  case: {
+                    $eq: ['$$this', 'B']
+                  },
+                  then: 4
+                },
+                {
+                  case: {
+                    $eq: ['$$this', 'C']
+                  },
+                  then: 3
+                },
+                {
+                  case: {
+                    $eq: ['$$this', 'D']
+                  },
+                  then: 2
+                }
+              ],
+              default: 1
+            }
+          }
+        }
+      }
+    })
+    .addFields({
+      avgScore: {
+        $round: [
+          {
+            $avg: '$avgScore'
+          }
+        ]
+      }
+    })
+    .addFields({
+      avgScoreLetter: {
+        $switch: {
+          branches: [
+            {
+              case: {
+                $eq: ['$avgScore', 5]
+              },
+              then: 'A'
+            },
+            {
+              case: {
+                $eq: ['$avgScore', 4]
+              },
+              then: 'B'
+            },
+            {
+              case: {
+                $eq: ['$avgScore', 3]
+              },
+              then: 'C'
+            },
+            {
+              case: {
+                $eq: ['$avgScore', 2]
+              },
+              then: 'D'
+            }
+          ],
+          default: 'F'
+        }
+      }
+    })
   return data
 }
 
